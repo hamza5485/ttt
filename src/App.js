304ccbde2b62a73792762a685f516014b4cc3035
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-02-14 19:23:21 +0500

    added react.lazyload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Board from "./components/app/board"
+import React from 'react';
+const Board = React.lazy(() => import('./components/app/board'));
 
 const App = () => {
 
     return (
-        <div className="App">
-            <Board />
-        </div>
+        <React.Suspense fallback={
+            <div>
+                Loading...
+            </div>
+        }>
+            <div className="App">
+                <Board />
+            </div>
+        </React.Suspense>
     );
 };
 
